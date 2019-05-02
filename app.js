0449bfa87b6f3b829a60cfd3e468e2739613327c
mrifkyilyas <mrifkyilyas@gmail.com>
2019-05-02 22:20:21 +0700

    base

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const port = process.env.PORT
+const cors = require('cors')
+const router = require('./routes/index')
+mongoose.connect('mongodb://localhost/muzieeek',{useNewUrlParser:true})
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use('/',router)
+
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(port)
+})
+
